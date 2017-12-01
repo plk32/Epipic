703eb379c4ceba4908557a75df8b1f9be0e2b112
Guillaume <guillaume.normand@epita.fr>
2017-12-01 20:03:56 +0100

    add MySQL database to project

--- app/index.js
+++ app/index.js
@@ -5,6 +5,43 @@ const path = require('path');
 const fs = require('fs');
 const minify = require('express-minify');
 
+// MYSQL Connection
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: '3306',
+  user: "root",
+  password: "password",
+  database: "epipic"
+});
+
+
+// Creation de la base et de la table image
+
+/*
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  connection.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+*/
+
+/*
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE image (id INT AUTO_INCREMENT PRIMARY KEY, url text, caption VARCHAR(255))";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+*/
+
 app.use(minify());
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -14,6 +51,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+
+/*
 app.get('/api/pictures', (req, res) => {
   //let myImages = fs.readFileSync('./api/1500-random-images.json');
   let myImages = fs.readFileSync('./api/myimages.json');
@@ -43,10 +82,13 @@ app.get('/api/pictures', (req, res) => {
   }
   res.send({ 'pictures' : array });
 });
+*/
 
+
+/*
 app.post('/api/pictures', (req, res) => {
   if (req.body.picture == null) {
-    return res.sendStatus(500);
+  return res.sendStatus(500);
   }
 
   let myImages = fs.readFileSync('./api/myimages.json');
@@ -65,7 +107,9 @@ app.post('/api/pictures', (req, res) => {
   res.send('Picture posted');
   console.log(obj);
 });
+*/
 
+/*
 app.delete('/api/pictures/:id', (req, res) => {
   let myImages = fs.readFileSync('./api/myimages.json');
   let pictures = JSON.parse(myImages);
@@ -78,6 +122,68 @@ app.delete('/api/pictures/:id', (req, res) => {
 
   let picturesNew = JSON.stringify(pictures);
   fs.writeFileSync('./api/myimages.json', picturesNew);
+  res.send('DELETE request for id: ' + req.params.id);
+});
+*/
+
+// GET depuis la bdd MySQL
+app.get('/api/pictures', (req, res) => {
+
+  // Select des images de MySQL
+  connection.query("SELECT id, url, caption FROM image", function (err, result, fields) {
+    if (err) throw err;
+    //console.log(JSON.stringify(result));
+    pictures = JSON.parse(JSON.stringify(result));
+
+    let array = [];
+    let cursor = -1;
+    if (req.query.cursor != undefined) {
+      cursor = parseInt(req.query.cursor);
+    }
+    let max = cursor + parseInt(req.query.amount);
+    console.log('cursor: ' + cursor);
+
+    if (cursor > pictures.length - 1) {
+      return res.sendStatus(404);
+    }
+
+    for(let i = cursor + 1; (i <= max) && (i < pictures.length); i++) {
+      let obj = {}
+      obj.id = pictures[i].id;
+      //obj.index = pictures[i].index;
+      obj.picture = pictures[i].url;
+      obj.caption = pictures[i].caption;
+      array.push(obj);
+    }
+    res.send({ 'pictures' : array });
+  });
+});
+
+// POST dans la bdd MySQL
+app.post('/api/pictures', (req, res) => {
+
+  if (req.body.picture == null) {
+    return res.sendStatus(500);
+  }
+
+  // Ajout de l'image dans MySQL
+  var sql = "INSERT INTO image (url, caption) VALUES ?";
+  var values = [
+    [req.body.picture, req.body.caption]
+  ]
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+  });
+  res.send('Picture posted');
+});
+
+// DELETE dans la bdd MySQL
+app.delete('/api/pictures/:id', (req, res) => {
+
+  var sql = "DELETE FROM image WHERE id = " + req.params.id;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+  });
   res.send('DELETE request for id: ' + req.params.id);
 });
 
